f139a4be222a9b18c34890fa319342ca177b4e2f
Volodymyr Voloshyn <Volodymyr_Voloshyn@epam.com>
2018-02-02 14:18:38 +0200

    Cart components

--- src/app/Cart/cart-item/cart-item.component.ts
+++ src/app/Cart/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ViewChild, ElementRef, HostBinding, AfterViewInit } from '@angular/core';
 import { CartItem } from '../cart-item';
 import { CartService } from '../cart.service';
 import { FormsModule } from '@angular/forms';
@@ -8,18 +8,50 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.css']
 })
-export class CartItemComponent implements OnInit {
+export class CartItemComponent implements OnInit, AfterViewInit {
   @Input() cartItem: CartItem;
-  @Input() isSelected = false;
+  @ViewChild('inputCartItemQnt')
+  inputCartItemQnt: ElementRef;
+  ishovering: boolean;
+  isSelected = false;
 
-  constructor(private cartService: CartService) {
+  @HostListener('click', []) edit() {
+    if (!this.isSelected) {
+      this.isSelected = true;
+
+      // this.element.nativeElement.querySelector('input').focus(); - don't work
+
+      setTimeout(function () {
+        this.inputCartItemQnt.nativeElement.focus();
+      }.bind(this), 1);
+    }
+  }
+
+  @HostListener('mouseover') onMouseOver() {
+    this.ishovering = true;
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    this.ishovering = false;
+  }
+
+  constructor(private element: ElementRef, private cartService: CartService) {
 
   }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(){
+    console.log(this.inputCartItemQnt);
+
+  }
+
   deleteCartItem(): void {
     this.cartService.removeProduct(this.cartItem);
   }
-}
+
+    editFinished(): void {
+      this.isSelected = false;
+    }
+  }
