ba279402d08091abd06dffa1ade75998098d8a05
Volodymyr Voloshyn <Volodymyr_Voloshyn@epam.com>
2018-02-06 22:03:21 +0200

    Task3

--- src/app/Cart/cart.service.ts
+++ src/app/Cart/cart.service.ts
@@ -4,14 +4,19 @@ import { CartItem } from './cart-item';
 
 @Injectable()
 export class CartService {
-  cartItems: Array<CartItem>;
+  private cartItems: Array<CartItem>;
+  private itemsCount: number;
+  private cartTotal: number;
 
   constructor() {
     this.cartItems = new Array<CartItem>();
+    this.itemsCount  = 0;
+    this.cartTotal  = 0;
   }
 
   addProduct(product: IProduct, quantity: number): void {
     this.cartItems.push(new CartItem(product, quantity));
+    this.calculateCartData();
   }
 
   removeProduct(cartItem: CartItem): void {
@@ -19,14 +24,45 @@ export class CartService {
 
     if (index > -1) {
       this.cartItems.splice(index, 1);
+
+      this.calculateCartData();
     }
   }
 
-  getProducts(): Array<CartItem> { 
+  updateProductItem(cartItem: CartItem, quantity: number) {
+    let index = this.cartItems.indexOf(cartItem);
+
+    if (index > -1) {
+      this.cartItems.quantity = quantity;
+
+      this.calculateCartData();
+    }
+  }
+
+  clearCart(): void{
+    
+  }
+
+  getProducts(): Array<CartItem> {
     return this.cartItems;
   }
 
   getProductsCount(): number {
-    return this.cartItems.length;
+    return this.itemsCount;
+  }
+
+  getTotal(): number {
+    return this.cartTotal;
+  }
+
+  private calculateCartData(): void{
+let total = 0;  
+
+    for (let item of this.cartItems) {
+      total += item.quantity * item.product.price;
+    }
+
+    this.itemsCount  = this.cartItems.length;
+    this.cartTotal  = total;
   }
 }
